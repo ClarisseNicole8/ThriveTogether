29e9875b59fe3fb6059211eb7387e50ecaa75ff2
wsrn829 <wsrn829@gmail.com>
2023-07-12 16:58:39 -0700

    get_peers models,queries,routers,main complete

---
+++ api/peers/routers/peers.py
@@ -0,0 +1,40 @@
+from typing import List
+from fastapi import APIRouter, Depends, Response
+
+from ..models import Peer
+
+from ..queries.peers import PeerQueries
+
+router = APIRouter()
+
+
+@router.get("/api/peers", response_model=List[Peer])
+def get_peers(
+    response: Response,
+    queries: PeerQueries = Depends(),
+):
+    records = queries.get_peers()
+
+    if records is None:
+        response.status_code = 404
+        return []
+
+    peers = []
+    for record in records:
+        peer = Peer(
+            id=record["id"],
+            username=record["username"],
+            name=record["name"],
+            age=record["age"],
+            gender=record["gender"],
+            pronouns=record["pronouns"],
+            profile_link=record["profile_link"],
+            profile_image=record["profile_image"],
+            banner_image=record["banner_image"],
+            email=record["email"],
+            about_me=record["about_me"],
+            my_story=record["my_story"],
+            preferences=record["preferences"],
+        )
+        peers.append(peer)
+    return peers
