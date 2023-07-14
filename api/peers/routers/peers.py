ceda4d29504ef9c474ceb9ed452a19bc341322c6
liangjianchen <liangjianchen@suumail.net>
2023-07-14 09:48:28 -0700

    alter table peer_connections

--- api/peers/routers/peers.py
+++ api/peers/routers/peers.py
@@ -38,3 +38,35 @@ def get_peers(
         )
         peers.append(peer)
     return peers
+
+
+@router.get("/api/peers/{user_id}", response_model=List[Peer])
+def get_peers_pending(
+    response: Response,
+    queries: PeerQueries = Depends(),
+):
+    records = queries.get_peers()
+
+    if records is None:
+        response.status_code = 404
+        return []
+
+    peers = []
+    for record in records:
+        peer = Peer(
+            id=record["id"],
+            username=record["username"],
+            name=record["name"],
+            age=record["age"],
+            gender=record["gender"],
+            pronouns=record["pronouns"],
+            profile_link=record["profile_link"],
+            profile_image=record["profile_image"],
+            banner_image=record["banner_image"],
+            email=record["email"],
+            about_me=record["about_me"],
+            my_story=record["my_story"],
+            preferences=record["preferences"],
+        )
+        peers.append(peer)
+    return peers
