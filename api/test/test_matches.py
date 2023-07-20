5420f0ed811b829815b228e07754a8e30273d642
TannerWJackson <tannerwenjiaojackson@gmail.com>
2023-07-20 21:53:34 +0000

    Resolve "Tag and match unit tests"

--- api/test/test_matches.py
+++ api/test/test_matches.py
@@ -1,23 +1,86 @@
 from fastapi.testclient import TestClient
 from main import app
 from matching.queries.matching import MatchQueries
+from pydantic import BaseModel
+from authenticator import authenticator
 
 client = TestClient(app)
 
 
+class AccountOut(BaseModel):
+    id: int
+    username: str
+    name: str
+    age: int
+    gender: str
+    pronouns: str
+    email: str
+    profile_image: str | None
+    banner_image: str | None
+    about_me: str | None
+    my_story: str | None
+    preferences: str | None
+
+
+fake_match = {
+                "username": "mmorales",
+                "id": 36,
+                "tags": [
+                    "ADHD",
+                    "Anxiety"
+                ],
+                "about_me": "string",
+                "profile_link": "string",
+                "profile_image": "string",
+                "gender": "Male",
+                "pronouns": "he/him"
+            }
+
+
 class EmptyMatchQueries:
-    def get_matches(self):
-        return []
+    def get_matches(self, tag):
+        if tag in fake_match["tags"]:
+            return {"matches": [fake_match]}
+        else:
+            return {"matches": []}
 
 
-# # Currently nonfunctional:
-# # endpoint is protected and I'm not sure how to test for that.
-# def test_get_matches():
-#     app.dependency_overrides[MatchQueries] = EmptyMatchQueries
+def fake_get_current_account_data():
+    return AccountOut(
+        id=0,
+        username="string",
+        name="string",
+        age=0,
+        gender="string",
+        pronouns="string",
+        email="string",
+        profile_image="string",
+        banner_image="string",
+        about_me="string",
+        my_story="string",
+        preferences="string",
+        )
 
-#     response = client.get("/api/matches")
 
-#     app.dependency_overrides = {}
+def test_get_matches():
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+    app.dependency_overrides[MatchQueries] = EmptyMatchQueries
 
-#     assert response.status_code == 200
-#     assert response.json() == {"matches": []}
+    response = client.get("/api/matches/ADHD/")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == {"matches": [fake_match]}
+
+
+def test_no_matches():
+    app.dependency_overrides[authenticator.get_current_account_data] = fake_get_current_account_data
+    app.dependency_overrides[MatchQueries] = EmptyMatchQueries
+
+    response = client.get("/api/matches/null/")
+
+    app.dependency_overrides = {}
+
+    assert response.status_code == 200
+    assert response.json() == {"matches": []}
