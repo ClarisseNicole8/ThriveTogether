ad9e6bd356b1a539d0985ad8f1e4f67f952832be
liangjianchen <liangjianchen@suumail.net>
2023-07-26 10:48:04 -0700

    fixed bug

--- api/test/test_peer_connection.py
+++ api/test/test_peer_connection.py
@@ -1,9 +1,30 @@
 from fastapi.testclient import TestClient
 from main import app
+from peers.queries.peers import PeerQueries
 client = TestClient(app)
 
 
-def test_get_peerConnection():
-    response = client.get("/api/peer_connections/100/")
+class EmptyPeerQueries:
+    def get_peer_request(self, user_id: int):
+        return {
+             "peerConnections": [
+                {
+                    "sender": 0,
+                    "recipient": 0,
+                    "status": "string",
+                    "has_messaged": "string",
+                    "sender_name": "string",
+                    "recipient_name": "string"
+                }
+             ]
+        }
+
+
+def test_get_peer_request():
+    app.dependency_overrides[PeerQueries] = EmptyPeerQueries
+    response = client.get("/api/peer_connections/1/")
+    app.dependency_overrides = {}
     assert response.status_code == 200
-    assert response.json() == {"peerConnections": []}
+    assert response.json() == {
+            "peerConnections": []
+        }
