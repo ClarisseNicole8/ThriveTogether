96465d07248ea7b9c32aaca38f644cc4b454fac9
Amanda <agifford887@gmail.com>
2023-07-26 11:52:30 -0700

    stashing changes

--- ghi/src/PeerButton.js
+++ ghi/src/PeerButton.js
@@ -1,9 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 
 function PeerButton(props) {
   const [addSuccess, setAddSuccess] = useState(false);
+  const [peerId, setPeerId] = useState("");
+  const [connectionExistsLoaded, setConnectionExistsLoaded] = useState(false);
+  const [connectionExists, setConnectionExists] = useState(false);
+  const [addAlert, setAddAlert] = useState(false);
 
-  async function handleAddPeer(event) {
+  async function connectionExistsFunc () {
+    const peerUrl = `${process.env.REACT_APP_API_HOST}/api/peer_connections/${peerId}`;
+    const fetchConfig = {
+      method: "get",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    const response = await fetch(peerUrl, fetchConfig);
+    let data = await response.json();
+    console.log("This is data", data);
+    if (response.ok) {
+      for (const connection in data.peerConnections) {
+        if (connection.recipient === peerId) {
+          setConnectionExists(true);
+        }
+      }
+    }
+    setConnectionExistsLoaded(true);
+  }
+
+  const handleAddPeer = useCallback(async (event) => {
     event.preventDefault();
     const data = {
       sender: props.senderId,
@@ -13,31 +38,51 @@ function PeerButton(props) {
       sender_name: props.senderName,
       recipient_name: props.recipientName,
     };
-    const peerUrl = `${process.env.REACT_APP_API_HOST}/api/connections/create/`;
-    const fetchConfig = {
-      method: "post",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    };
-    const response = await fetch(peerUrl, fetchConfig);
-    if (response.ok) {
-      setAddSuccess(true);
+
+    setPeerId(data.recipient);
+
+    // setConnectionExists(connectionExistsFunc(peerId));
+    if (!connectionExists && connectionExistsLoaded) {
+      const peerUrl = `${process.env.REACT_APP_API_HOST}/api/connections/create/`;
+      const fetchConfig = {
+        method: "post",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      const response = await fetch(peerUrl, fetchConfig);
+      if (response.ok) {
+        setAddSuccess(true);
+      }
+      } else {
+        setAddAlert(true);
+      }
+  }, [props.senderId, props.recipientId, props.senderName, props.recipientName, setPeerId, setConnectionExists, connectionExists, connectionExistsLoaded])
+
+  useEffect(() => {
+    if (peerId) {
+      connectionExistsFunc();
     }
-  }
+  }, [peerId]);
 
   return (
-    <div>
-      {addSuccess ? (
-        <div className="btn btn-primary">Success!</div>
-      ) : (
-        <button className="btn btn-primary" onClick={handleAddPeer}>
-          Add Peer
-        </button>
-      )}
-    </div>
-  );
+  <div>
+    {addAlert ? (
+      <div className="alert alert-warning" role="alert">
+        The request has already been made!
+      </div>
+    ) : addSuccess ? (
+      <div className="alert alert-success" role="alert">
+        Success!
+      </div>
+    ) : (
+      <button className="btn btn-primary" onClick={handleAddPeer}>
+        Add Peer
+      </button>
+    )}
+  </div>
+);
 }
 
 export default PeerButton;
