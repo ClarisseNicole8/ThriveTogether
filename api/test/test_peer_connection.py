540c713ee8138905ecd3bbf19a29b2a72850b4c6
liangjianchen <liangjianchen@suumail.net>
2023-07-26 11:41:36 -0700

    fix bugs

--- api/test/test_peer_connection.py
+++ api/test/test_peer_connection.py
@@ -1,30 +1,37 @@
 from fastapi.testclient import TestClient
 from main import app
-from peers.queries.peers import PeerQueries
+from peers.routers.peers import PeerConnectionQueries
 client = TestClient(app)
 
 
 class EmptyPeerQueries:
-    def get_peer_request(self, user_id: int):
-        return {
-             "peerConnections": [
-                {
-                    "sender": 0,
-                    "recipient": 0,
-                    "status": "string",
-                    "has_messaged": "string",
-                    "sender_name": "string",
-                    "recipient_name": "string"
-                }
-             ]
-        }
+    def get_peer_connection(self, user_id: int):
+        return [
+                    {
+                        "sender": 0,
+                        "recipient": 0,
+                        "status": "string",
+                        "has_messaged": "string",
+                        "sender_name": "string",
+                        "recipient_name": "string"
+                    }
+                ]
 
 
-def test_get_peer_request():
-    app.dependency_overrides[PeerQueries] = EmptyPeerQueries
+def test_get_peer_connection():
+    app.dependency_overrides[PeerConnectionQueries] = EmptyPeerQueries
     response = client.get("/api/peer_connections/1/")
     app.dependency_overrides = {}
     assert response.status_code == 200
     assert response.json() == {
-            "peerConnections": []
-        }
+                "peerConnections": [
+                    {
+                        "sender": 0,
+                        "recipient": 0,
+                        "status": "string",
+                        "has_messaged": "string",
+                        "sender_name": "string",
+                        "recipient_name": "string"
+                    }
+                ]
+    }
