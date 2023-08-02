833f3e4c53f8dc5c83b14eba1042c66c6163f4e2
TannerWJackson <tannerwenjiaojackson@gmail.com>
2023-08-02 19:34:46 +0000

    Resolve "Login refactoring"

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -5,6 +5,7 @@ const LoginForm = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useToken();
+  const { token } = useToken();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -14,29 +15,36 @@ const LoginForm = () => {
 
   return (
     <div className="content-container bg-text rounded-edges d-flex justify-content-center">
-      <form onSubmit={(e) => handleSubmit(e)} style={{width: "750px"}}>
-        <div className="mb-3">
-          <label className="form-label">Username:</label>
-          <input
-            name="username"
-            type="text"
-            className="form-control"
-            onChange={(e) => setUsername(e.target.value)}
-          />
+      {!token &&
+        <form onSubmit={(e) => handleSubmit(e)} style={{width: "750px"}}>
+          <div className="mb-3">
+            <label className="form-label">Username:</label>
+            <input
+              name="username"
+              type="text"
+              className="form-control"
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </div>
+          <div className="mb-3">
+            <label className="form-label">Password:</label>
+            <input
+              name="password"
+              type="password"
+              className="form-control"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="d-flex justify-content-center">
+            <input className="btn btn-primary" type="submit" value="Login" />
+          </div>
+        </form>
+      }
+      {token &&
+        <div>
+          <h3>Already logged in!</h3>
         </div>
-        <div className="mb-3">
-          <label className="form-label">Password:</label>
-          <input
-            name="password"
-            type="password"
-            className="form-control"
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <div className="d-flex justify-content-center">
-          <input className="btn btn-primary" type="submit" value="Login" />
-        </div>
-      </form>
+      }
     </div>
   );
 };
--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -1,10 +1,30 @@
 import logo from "./images/thrivetogether.png";
 import useToken from "@galvanize-inc/jwtdown-for-react";
 import { NavLink, Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 function Nav() {
+    const [userData, setUserData] = useState({});
     const { logout } = useToken();
     const { token } = useToken();
+
+    useEffect(() => {
+        async function getUserData() {
+        let url = `${process.env.REACT_APP_API_HOST}/token`;
+        let response = await fetch(url, {
+            credentials: "include",
+        });
+        let data = await response.json();
+
+        if (response.ok) {
+            setUserData(data.account);
+        } else {
+            console.log("User data could not be fetched");
+        }
+        }
+
+        getUserData();
+    }, [token]);
 
     return (
         <>
@@ -32,11 +52,18 @@ function Nav() {
                                         </>
                                     }
                                     {token &&
-                                    <li className="nav-item">
-                                        <button className="nav-link" onClick={logout}>
-                                            Logout
-                                        </button>
-                                    </li>
+                                    <>
+                                        <li className="nav-item">
+                                            <div className="nav-link" style={{width: "250px"}}>
+                                                Welcome, {userData.name}
+                                            </div>
+                                        </li>
+                                        <li className="nav-item">
+                                            <button className="nav-link" onClick={logout}>
+                                                Logout
+                                            </button>
+                                        </li>
+                                    </>
                                     }
                                 </ul>
                             </div>
@@ -55,13 +82,13 @@ function Nav() {
                             <Link to="/matches">Matches</Link>
                         </li>
                         <li>
-                            <Link to="/peers">Peers</Link>
-                        </li>
-                        <li>
                             <Link to="/edit_tags">Edit Tags</Link>
                         </li>
                         <li>
-                            <Link to="/peer_connections">Check peer request</Link>
+                            <Link to="/peers">Peers</Link>
+                        </li>
+                        <li>
+                            <Link to="/peer_connections">Peer Requests</Link>
                         </li>
                     </ul>
                 </div>
